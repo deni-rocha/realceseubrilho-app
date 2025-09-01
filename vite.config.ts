0c84fe494749900cd84938fded6b686ef2377859
deni-rocha <denilsonrocha_@outlook.com>
2025-09-01 20:44:58 -0300

    desativa cache pwa no modo dev

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
       devOptions: {
-        enabled: true,
+        enabled: false,
       },
       manifest: {
         name: 'Realce Seu Brilho',
